c67b884fc2568e31de134b232e44959040f3f936
Jake Romer <mail@jakeromer.com>
2020-05-12 19:39:54 -0400

    Refactor change maker

--- app/services/change_maker.rb
+++ app/services/change_maker.rb
@@ -2,51 +2,72 @@
 
 module ChangeMaker
   class << self
-    # Render a target `amount` of change.
+    # Render a target `amount` of change, subject to a denomination availability
+    # constraint represented by `till`.
+    #
     # Expects and returns integer currency values.
     #
     # Parameters:
+    #
     # - amount: Integer. The total amount of change to render
-    # - till: Hash. Mapping available denominations to quantities
+    # - till: Hashish. Map or tuple list of the form `{denomination => quantity`
     # - trials: Integer. The number of attempts to make
     #
-    # Return a Hash mapping denomination amounts to quantities
+    # Note that the first trial takes the greedy approach, while subsequent
+    # trials are randomized. I may refactor to implement the asymptotically
+    # optimal solution, but:
     #
-    # Return an empty Hash if the target amount cannot be rendered with the
-    # given denominations
+    # 1. This solution is expedient to implement, easy to understand, and
+    #    bounded by fixed parameter magnitudes (`till`, `trials`) such that it
+    #    has a net cost advantage over the optimum solution.
+    #
+    # 2. As always, the performance difference between asymptotically optimal
+    #    and suboptimal implementations isn't guaranteed to be monotonic over
+    #    the entire range of inputs, so a comparison benchmark would be needed
+    #    to determine optimality given the concrete constraints of the
+    #    application.
+    #
+    # Return a Hash, possibly empty, mapping denomination amounts to quantities
     def render_change(amount, till, trials = 15)
       change_set = Set.new
+      till = till.sort_by(&:first).reverse
 
       trials.times do |trial|
-        change = Hash.new(0)
-        balance = amount
-        money = trial.zero? ? till.sort_by(&:first).reverse : till.to_a.shuffle
-
-        money.each do |denomination, qty|
-          break if balance.zero?
-
-          remaining_qty = qty
-
-          while balance.positive?
-            break if denomination > balance
-            break if remaining_qty.zero?
-
-            balance -= denomination
-            change[denomination] += 1
-
-            remaining_qty -= 1
-          end
-        end
-
-        change_set.add(change) if balance.zero?
+        denomination_qtys = trial.zero? ? till : till.shuffle
+        change_set.add try_making_change(amount, denomination_qtys)
       end
 
+      # Return least by number of denominations
+      # NB: nil.to_h returns {}
       change_set
+        .to_a
+        .compact
         .min_by { |change| change.values.sum }
         .to_h
-        .map { |denom, qty| [denom, qty] }
         .sort_by { |denom, _| -denom }
         .to_h
+    end
+
+    def try_making_change(amount, till, change=Hash.new(0))
+      remaining_amt = amount
+
+      till.each do |denomination, qty|
+        break if remaining_amt.zero?
+
+        remaining_qty = qty
+
+        while remaining_amt.positive?
+          break if denomination > remaining_amt
+          break if remaining_qty.zero?
+
+          remaining_amt -= denomination
+          change[denomination] += 1
+
+          remaining_qty -= 1
+        end
+      end
+
+      return change if remaining_amt.zero?
     end
   end
 end
